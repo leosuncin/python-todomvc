36a5556ac9d9643e98f2a40f7901568211959372
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2023-12-14 10:44:52 -0600

    feat: add first endpoint

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+
+class Greet(BaseModel):
+    message: str
+
+
+@app.get("/greeting", response_model=Greet)
+def say_hi(name: str = "World"):
+    return {"message": f"Hello {name}"}
